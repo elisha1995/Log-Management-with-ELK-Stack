17e68bb7cf9d6a313f1d352c76bab4be6ca15513
elisha1995 <elishasamuel01@gmail.com>
2024-10-04 07:24:05 +0000

    Demonstrating log analysis Visualizations in Kibana with Elasticsearch and Logstash

---
+++ src/main/java/ges/elk/LoggingDemoApplication.java
@@ -0,0 +1,31 @@
+package ges.elk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LoggingDemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingDemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
+
